de4b02806efd21993d020a10bd379981473d5970
Laurent Brucher <laurent.brucher@idelis.be>
2011-11-10 23:23:16 -0500

    fixed tests

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -18,7 +18,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, :user => @user.attributes
+      post :create, :user => {:username => "joe3", :password => "secret3", :fullname => "whatever"}
     end
 
     assert_redirected_to user_path(assigns(:user))
